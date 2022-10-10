d55cabbc9082ebdfbc3a87ef9fca07408e4e6749
Daniel <dduranf83@gmail.com>
2022-10-10 20:50:11 -0300

    agregando js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 let nombre;
 let origen;
 let destino;
-const precio = 100;
+const precio = 50;
 
 function inicio(){
     alert("Bienvenido a Forfly Viaggi! Donde deseas viajar?")
 }
 inicio()
 
-function mostrarViaje(){
+function mostrarViajeIda(){
     let nombre = prompt("Ingresa tu nombre?");
     let origen = prompt ("Ingresar origen");
     let destino = prompt ("Ingresa destino");
@@ -16,7 +16,29 @@ function mostrarViaje(){
     alert(`Hola ${nombre.toUpperCase()} tu reserva es: origen ${origen.toUpperCase()} destino ${destino.toUpperCase()}`);
 }
 
-mostrarViaje()
+function regreso(){
+    alert (`Desea comprar el pasaje de regreso`);
+    let opcionRegreso = prompt("Desea comprar el pasaje de 1: SI 2: NO");
+    if (opcionRegreso == "si"){
+        alert ("Continuemos entonces");
+    }
+    else {
+        alert ("Opcion cancelada");
+    }
+
+}
+mostrarViajeIda();
+regreso ();
+
+function mostrarViajeVuelta(){
+    let nombre = prompt("Ingresa tu nombre?");
+    let origen = prompt ("Ingresar origen");
+    let destino = prompt ("Ingresa destino");
+   
+    alert(`Hola ${nombre.toUpperCase()} tu reserva es: origen ${origen.toUpperCase()} destino ${destino.toUpperCase()}`);
+}
+
+mostrarViajeVuelta();
 
 function pago (){
     let formaDePago = parseInt(prompt("Forma de pago 1: Efectivo 2: Tarjeta"));
@@ -47,4 +69,58 @@ function pago (){
 }
 pago()
 
+inicio();
 
+const listaDestino = [
+    { nombre: "buenos aires", precio: 30},
+    { nombre: "jujuy", precio: 20},
+    { nombre: "salta", precio: 10},
+    { nombre: "tucuman", precio: 10},
+    { nombre: "santiago del estero", precio: 10},
+    { nombre: "la rioja", precio: 10},
+    { nombre: "mendoza", precio: 15},
+    { nombre: "san juan", precio: 10},
+    { nombre: "chubut", precio: 20},
+    { nombre: "tierra del fuego", precio: 20},
+    { nombre: "catamarca", precio: 10},
+    { nombre: "chaco", precio: 10},
+    { nombre: "formosa", precio: 10},
+    { nombre: "misiones", precio: 10},
+    { nombre: "corrientes", precio: 10},
+    { nombre: "santa fe", precio: 10},
+    { nombre: "entre rios", precio: 10},
+    { nombre: "cordoba", precio: 10},
+    { nombre: "san luis", precio: 15},
+    { nombre: "neuquen", precio: 15},
+    { nombre: "rio negro", precio: 15},
+    { nombre: "la pampa", precio: 10},
+    { nombre: "santa cruz", precio: 20},
+  
+];
+
+listaDestino.forEach((destino)=> {
+    console.log(`Su destino es ${destino.nombre} y su precio es ${destino.precio} USDT`)
+})
+ 
+let buscarPorPrecio = listaDestino.filter(destino => destino.precio > 30);
+
+console.log(buscarPorPrecio)
+
+ 
+class Nuevo {
+    constructor(nombre, precio,) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+const crearDestino = ()=> {
+    let nombreDestino = prompt("Agregue nuevo destino");
+    let precioDestino = prompt("Precio que quiere pagar");
+
+
+    const destinoNuevo = new Nuevo(nombreDestino,precioDestino);
+    console.log(destinoNuevo);
+    return destinoNuevo;
+}
+crearDestino()
